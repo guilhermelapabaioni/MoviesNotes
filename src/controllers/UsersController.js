868307aad26983af564b13885a0bbfc3e61a3ffb
Guilherme Lapa Baioni <guilherme.lapa.baioni@outlook.com>
2022-08-24 19:35:16 -0300

    Update class UsersControllers, with methods update, show, index and delete, configure the route users with new methods.

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,6 @@
 const sqliteConnection = require('../database/sqlite')
 const AppError = require('../utils/AppError')
-const {hash} = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 
 const regex =
   /^(?=(?:.*?[A-Z]){1})(?=(?:.*?[0-9]){1})(?=(?:.*?[!@#$%*()_+^&}{:;?.]){1})(?!.*\s)[0-9a-zA-Z!@#$%;*(){}_+^&]*$/
@@ -26,6 +26,7 @@ class UsersController {
 
     // Validando se o email informado no corpo já foi registrado anteriormente.
     if (checkIfUserExists) {
+      ;('')
       throw new AppError('E-mail already registered.')
     }
 
@@ -49,6 +50,109 @@ class UsersController {
     )
 
     return response.json()
+  }
+
+  async update(request, response) {
+    const { id } = request.params
+    const { name, email, oldPassword, newPassword } = request.body
+
+    const database = await sqliteConnection()
+
+    // Buscando as informações do usuário cujo o ID informado.
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+    // Validando se o usuário está cadastrado no banco de dados.
+    if (!user) {
+      throw new AppError('User not registred.')
+    }
+
+    // Buscando as informações do usuário cujo o Email informado.
+    const userCheckUpdateEmail = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
+
+    // Validando se o e-mail informado está cadastrado no banco de dados.
+    if (userCheckUpdateEmail) {
+      // Validando se o e-mail informado está sendo utilizado por outro usuário.
+      if (userCheckUpdateEmail.id !== user.id) {
+        throw new AppError('This e-mail already used for other user.')
+      }
+      throw new AppError('This e-mail already used for you.')
+    }
+
+    // Validando se a senha antiga foi inserida.
+    if (!oldPassword && newPassword) {
+      throw new AppError('You need inform your old password.')
+      // Validando se a nova senha foi inserida.
+    } else if (oldPassword && !newPassword) {
+      throw new AppError('You need inform your new password.')
+    }
+
+    // Validando se a senha antiga informada corresponde a senha cadastrada no banco de dados.
+    if (oldPassword && newPassword) {
+      const checkPassword = await compare(oldPassword, user.password)
+      if (!checkPassword) {
+        throw new AppError('Your old password is incorrect.')
+      }
+      if (newPassword.length < 8) {
+        throw new AppError(
+          'Your password needs 8 or more characters, one number and one special character.'
+        )
+        // Validando se a senha informada para cadastro possui os parâmetros necessários informados pelo regex.
+      } else if (!regex.exec(newPassword)) {
+        throw new AppError(
+          'Your password needs 8 or more characters, one number and one special character.'
+        )
+      }
+      user.password = await hash(newPassword, 12)
+    }
+
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    await database.run(
+      `UPDATE users SET name = ?, email = ?, password = ?, updated_at = DATETIME('now') WHERE id = ?`,
+      [user.name, user.email, user.password, id]
+    )
+
+    return response.json()
+  }
+
+  async show(request, response) {
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+
+    const user = await database.get(
+      'SELECT id, name, email FROM users WHERE id = (?)',
+      [id]
+    )
+
+    return response.json(user)
+  }
+
+  async index(request, response) {
+    const { name, email } = request.query
+
+    const database = await sqliteConnection()
+
+    const user = await database.get(
+      'SELECT id, name, email FROM users WHERE name = (?) OR email = (?)',
+      [name, email]
+    )
+
+    return response.json(user)
+  }
+
+  async delete(request, response) {
+    const {id} = request.params
+
+    const database = await sqliteConnection()
+
+    const user = await database.run('DELETE FROM users WHERE id = (?)', [id])
+
+    return response.json(user)
   }
 }
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,5 +5,9 @@ const usersRoutes = Router()
 const usersController = new UsersController()
 
 usersRoutes.post('/', usersController.create)
+usersRoutes.put('/:id', usersController.update)
+usersRoutes.get('/:id', usersController.show)
+usersRoutes.get('/', usersController.index)
+usersRoutes.delete('/:id', usersController.delete)
 
 module.exports = usersRoutes
